46d1c1f87cc21b3261446144197af8756fe231c5
Chamathka Sewwandi <112297760+chamathkas1227@users.noreply.github.com>
2023-12-04 20:28:54 +0530

    Update urls.py
    Imported 'SaveContact' view

--- MTB/urls.py
+++ MTB/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, MovieListView, AboutView, ContactView, SeatLayoutView
+from .views import HomeView, MovieListView, AboutView, ContactView, SeatLayoutView, SaveContact
 
 app_name = 'MTB'
 
